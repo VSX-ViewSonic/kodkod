ac4cfa2ce2c4d9b32654145c98fe780f364982d3
Tony Huang <tony.ch.huang@viewsonic.com>
2024-04-24 15:45:48 +0800

    chore: add db in commit message types

--- kodkod/pre_commit_hooks/check_commit_message.py
+++ kodkod/pre_commit_hooks/check_commit_message.py
@@ -3,14 +3,19 @@ import sys
 
 
 def main() -> int:
-
-    with open(".git/COMMIT_EDITMSG", 'r') as file:
+    with open(".git/COMMIT_EDITMSG", "r") as file:
         first_line = file.readline().strip()
     # Define the regex pattern for checking the commit message
-    pattern = r'^(\[\w{9}\])+ (feat|chore|fix|ci|refactor|test|style|build|docs)(\(\w*\))?!?: .'
+    pattern = r"^(\[\w{9}\])+ (feat|chore|fix|ci|refactor|test|style|build|docs|db)(\(\w*\))?!?: ."
     if not re.match(pattern, first_line):
-        print("Commit message should have ClickUp ticket id and types(feat|chore|fix|ci|refactor|test|style|build|docs).", file=sys.stderr)
-        print("Please follow https://www.conventionalcommits.org/en/v1.0.0/", file=sys.stderr)
+        print(
+            "Commit message should have ClickUp ticket id and types(feat|chore|fix|ci|refactor|test|style|build|docs|db).",
+            file=sys.stderr,
+        )
+        print(
+            "Please follow https://www.conventionalcommits.org/en/v1.0.0/",
+            file=sys.stderr,
+        )
         print("Required format:", file=sys.stderr)
         print("    [ticketid] feat: commit message", file=sys.stderr)
         print("    [ticketid] feat!: breaking change", file=sys.stderr)
@@ -20,5 +25,6 @@ def main() -> int:
 
     return 0
 
+
 if __name__ == "__main__":
-    raise SystemExit(main())
+    exit(main())
